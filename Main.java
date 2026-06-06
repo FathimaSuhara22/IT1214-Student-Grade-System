38b7691bec3b7e53f1f6befcce4354f3681e9fe8
FathimaSuhara22 <thahirsuhara479@gmail.com>
2026-06-06 19:38:16 +0530

    Add student input functionality
    Implemented functionality to add students with ID, name, and marks.

--- Main.java
+++ Main.java
@@ -1,5 +1,46 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+
+    static ArrayList<Student> students = new ArrayList<>();
+    static Scanner input = new Scanner(System.in);
+
+    public static void addStudent() {
+
+        System.out.print("Enter Student ID: ");
+        String id = input.nextLine();
+
+        System.out.print("Enter Student Name: ");
+        String name = input.nextLine();
+
+        System.out.print("Enter Marks: ");
+        double marks = input.nextDouble();
+        input.nextLine();
+
+        students.add(new Student(id, name, marks));
+
+        System.out.println("Student Added Successfully!");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Student Grade Management System");
+
+        int choice;
+
+        do {
+            System.out.println("\n1. Add Student");
+            System.out.println("2. Exit");
+            System.out.print("Enter Choice: ");
+
+            choice = input.nextInt();
+            input.nextLine();
+
+            switch (choice) {
+                case 1:
+                    addStudent();
+                    break;
+            }
+
+        } while (choice != 2);
     }
 }
